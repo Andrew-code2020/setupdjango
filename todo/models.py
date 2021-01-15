91a76db5138a81f6c118b821a5ef596db33906b3
Andrew Burns <burnsad@tcd.ie>
2021-01-15 16:05:46 +0000

    Rendering Data  part 2 complete

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here with built model class from model.
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
