b83cd2b328f25920fd13870558e5152ac22eb2b8
Andrew Burns <burnsad@tcd.ie>
2021-01-15 11:43:01 +0000

    model1 video complete and code added to models.py

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here with built model class from model.
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
