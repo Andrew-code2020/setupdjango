43b35bddb7e21eb570d46041bacb4716a9d0d0f4
Andrew Burns <burnsad@tcd.ie>
2021-01-22 15:13:15 +0000

    SECRET KEY removed and repositioned

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    'SECRET_KEY', '3jp6$=$xi3xdwhrmhh9zjijg(5gjq3l7s$^(zkx9xbgdcyg+g*')
+    'SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
