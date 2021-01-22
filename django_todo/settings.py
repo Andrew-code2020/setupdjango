a61bc37c6f50521ff13262b6e85c55aa46e73a4b
Andrew Burns <burnsad@tcd.ie>
2021-01-22 13:18:14 +0000

    adjusted settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get ('SECRET_KEY','3jp6$=$xi3xdwhrmhh9zjijg(5gjq3l7s$^(zkx9xbgdcyg+g*')
+SECRET_KEY =  os.environ.get('SECRET_KEY','3jp6$=$xi3xdwhrmhh9zjijg(5gjq3l7s$^(zkx9xbgdcyg+g*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get ['HEROKU_HOSTNAME']
+ALLOWED_HOSTS = [os.environ.get ('HEROKU_HOSTNAME')]
 
 
 # Application definition
